ca61dbff88544e6a98f09703ea03417456f809c6
Miguel A. Montoya Z. <miguel.mzbi@gmail.com>
2016-08-15 21:12:40 -0500

    HW 1 1.1

--- src/RecString.java
+++ src/RecString.java
@@ -1,15 +1,11 @@
-
 public class RecString {
 
 	public static int StrLen(String s){
-		if(s.length()==1){
-			return 1;
-		}
-		else if(s.length() == 0){
+		if(s.equals("")||s==null){
 			return 0;
 		}
 		else{
-			return 1 + StrLen(s.substring(0, s.length()-1));
+			return 1+ StrLen(s.substring(1));
 		}
 	}
 }
