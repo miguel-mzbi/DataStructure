f999dd0f86b91a2ad0cb8d954e2feb60fbb4929c
Miguel A. Montoya Z. <miguel.mzbi@gmail.com>
2016-08-15 19:17:59 -0500

    HW 1 1.0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = RecString.StrLen("");
+		System.out.println(x);
+		x = RecString.StrLen("hello");
+		System.out.println(x);
+		x = RecString.StrLen("siaodpdahfiuoehfa");
+		System.out.println(x);
+	}
+
+}
