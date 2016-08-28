aec528ad86180b8c2dd72a5b573f9a1a10f5aff8
Miguel Angel <miguel.mzbi@gmail.com>
2016-08-27 22:24:51 -0500

    H1 2.2.3

--- src/h1/Peak.java
+++ src/h1/Peak.java
@@ -2,29 +2,28 @@ package h1;
 
 public class Peak {
 
-	public static int OneDRecursive(int[] a){
-		int n =a.length; //Obtain length
-		int i = (n%2==0)?(n/2):(n+1)/2; //Obtain middle index
-		return OneDRecursive(a, n, i); //Start recursion
+	public static int OneDRecursive(int[] a){	
+		return OneDRecursive(a, a.length, 0, a.length); //Start recursion
 	}
-	public static int OneDRecursive(int[] a, int n, int i){
-		if(n==0){ //Return 0 if array is empty
-			return 0;
+	public static int OneDRecursive(int[] a, int l, int leftI, int rightI){
+		int i =((rightI+leftI)%2==0)?((rightI+leftI)/2):(((rightI+leftI)+1)/2);
+		if(l==0){ //Return 0 if array is empty
+			return -1;
 		}
-		else if(i==n-1){ //Return right limit if already on right limit
-			return a[n-1];
+		else if(l==1){ //Return only value if array is of length one
+			return a[0];
+		}
+		else if(i==l-1){ //Return right limit if already on right limit
+			return a[l-1];
 		}
 		else if(i==0){ //Return left limit if already on left limit
 			return a[0];
 		}
-		else if(n==1){ //Return only value if array is of length one
-			return a[0];
-		}
 		else if(a[i-1]<=a[i] && a[i]<=a[i+1]){ //Move to the right
-			return OneDRecursive(a, n, i+1);
+			return OneDRecursive(a, l, i+1, rightI);
 		}
 		else if(a[i-1]>=a[i] && a[i]>=a[i+1]){ //Move to the left
-			return OneDRecursive(a, n, i-1);
+			return OneDRecursive(a, l, leftI, i-1);
 		}
 		else{ //Return peak
 			return a[i];
