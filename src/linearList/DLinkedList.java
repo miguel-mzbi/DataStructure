5189c0a9a678067d1f09e4265eafaf4bef02d815
Miguel A. Montoya Z. <miguel.mzbi@gmail.com>
2016-09-03 15:56:22 -0500

    Final DLinkedList

--- src/linearList/DLinkedList.java
+++ src/linearList/DLinkedList.java
@@ -13,14 +13,26 @@ public class DLinkedList<Item> implements LinearList<Item> {
 		this.size = 0;
 	}
 	
+	/**
+	 * isEmpy - Returns true if chain is empty
+	 * @return boolean true if empty false if not empty
+	 */
 	public boolean isEmpty() {
 		return this.size == 0;
 	}
-
+	
+	/**
+	 * size - Returns size
+	 * @return integer corresponding to size of DNose
+	 */
 	public int size() {
 		return this.size;
 	}
 
+	/**
+	 * get - Calls method to get the selected node. First checks for valid index
+	 * @return Value of selected node
+	 */
 	public Item get(int index) {
 		if(index<0 || index>=this.size){
 			throw new IndexOutOfBoundsException("Index out of range");
@@ -28,6 +40,10 @@ public class DLinkedList<Item> implements LinearList<Item> {
 		return this.getNode(index).element;
 	}
 	
+	/**
+	 * getNode - Returns node at index
+	 * @return DNode at index
+	 */
 	private DNode<Item> getNode(int index){
 		DNode<Item> temp = this.firstNode;
 		for(int i = 0; i<index; i++){
@@ -36,6 +52,9 @@ public class DLinkedList<Item> implements LinearList<Item> {
 		return temp;
 	}
 	
+	/**
+	 * addFirst - Adds a node with the selected Item at the start
+	 */
 	public void addFirst(Item a){
 		DNode<Item> temp = this.firstNode;
 		DNode<Item> newFirstNode = new DNode<Item>(null, a, this.firstNode);
@@ -49,6 +68,9 @@ public class DLinkedList<Item> implements LinearList<Item> {
 		this.size++;
 	}
 	
+	/**
+	 * addLast - Adds a node with the selected Item at the end
+	 */
 	public void addLast(Item a){
 		DNode<Item> temp = this.lastNode;
 		DNode<Item> newLastNode = new DNode<Item>(this.lastNode, a, null);
@@ -62,22 +84,69 @@ public class DLinkedList<Item> implements LinearList<Item> {
 		this.size++;
 	}
 
-	@Override
-	public int indexOf(Object item) {
-		// TODO Auto-generated method stub
-		return 0;
+	/**
+	 * indexOf - Returns the index of selected node
+	 * @return integer corresponding to the node's index
+	 */
+	public int indexOf(Item item) {
+		DNode<Item> temp = this.lastNode;
+		for(int i = 0; i<this.size; i++){
+			if(temp.element.equals(item)){
+				return i;
+			}
+			temp = temp.next;
+		}
+		return -1;
 	}
 
-	@Override
+	/**
+	 * remove - Returns the element of the selected node to remove
+	 * @return element of removed node
+	 */
 	public Item remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if(index<0 || index>this.size){
+			throw new IndexOutOfBoundsException("Index out of range");
+		}
+		DNode<Item> nodeRemove = this.firstNode;
+		if(index == 0){
+			this.firstNode = nodeRemove.next;
+			this.firstNode.prev = null;
+		}
+		else{
+			for(int i = 0; i<index;i++){
+				nodeRemove = nodeRemove.next;
+			}
+			DNode<Item> prv = nodeRemove.prev;
+			DNode<Item> nxt = nodeRemove.next;
+			prv.next = nxt;
+			nxt.prev = prv;
+		}
+		this.size--;
+		return nodeRemove.element;
 	}
 
-	@Override
-	public void add(int index, Object item) {
-		// TODO Auto-generated method stub
-		
+	public void add(int index, Item item) {
+		if(index<0 || index>this.size){
+			throw new IndexOutOfBoundsException("Index out of range");
+		}
+		if(index == 0){
+			addFirst(item);
+		}
+		else if(index == this.size){
+			addLast(item);
+		}
+		else{
+			DNode<Item> prv, nxt;
+			prv = this.firstNode;
+			for(int i = 0; i<index-1;i++){
+				prv=prv.next;
+			}
+			nxt=prv.next;
+			DNode<Item> newNode = new DNode<Item>(prv, item, nxt);
+			prv.next = newNode;
+			nxt.prev = newNode;
+			this.size++;
+		}
 	}
 
 	public String output() {
@@ -105,29 +174,25 @@ public class DLinkedList<Item> implements LinearList<Item> {
 		}
 	}
 
-	private class ListItr implements ListIterator<Item>{
-		private DNode<Item> lastVisitedNode = null;
-		private DNode<Item> next;
+	protected class ListItr implements ListIterator<Item>{
+		private DNode<Item> lastVisitedNode;
+		private DNode<Item> nxt;
 		private int nextIndex;
 		
 		public ListItr(){
-			this.next = firstNode;
+			this.lastVisitedNode = null;
+			this.nxt = DLinkedList.this.firstNode;
 			this.nextIndex = 0;
 		}
 		
 		public ListItr(int index){
 			if(index==size){
-				this.next = null;
+				this.nxt = null;
 			}
 			else{
-				this.next=getNode(index);
+				this.nxt=getNode(index);
 			}
 			this.nextIndex = index;
-		}
-		
-		public void add(Item arg0) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		public boolean hasNext() {
@@ -142,45 +207,50 @@ public class DLinkedList<Item> implements LinearList<Item> {
 			if(!hasNext()){
 				throw new NoSuchElementException("No elements");
 			}
-			this.lastVisitedNode = next;
-			this.next = this.next.next;
+			this.lastVisitedNode = this.nxt;
+			this.nxt = this.nxt.next;
 			this.nextIndex++;
 			return this.lastVisitedNode.element;
 		}
 
-		@Override
 		public int nextIndex() {
-			// TODO Auto-generated method stub
-			return 0;
+			if(!hasNext()){
+				return size;
+			}
+			return this.nextIndex++;
 		}
 
 		public Item previous() {
 			if(!hasPrevious()){
 				throw new NoSuchElementException("No elements");
 			}
-			if(this.next==null){
-				this.next = this.lastVisitedNode;
+			if(this.nxt==null){
+				this.nxt = this.lastVisitedNode;
 			}
 			else{
-				this.next = this.next.prev;
+				this.nxt = this.nxt.prev;
 			}
-			this.lastVisitedNode=this.next;
+			this.lastVisitedNode=this.nxt;
 			this.nextIndex--;
 			return this.lastVisitedNode.element;
 		}
 
-		@Override
 		public int previousIndex() {
-			// TODO Auto-generated method stub
-			return 0;
+			if(!hasPrevious()){
+				return -1;
+			}
+			return this.previousIndex();
 		}
 
 		public void remove() {
 			// TODO Auto-generated method stub
 			
 		}
-
 		public void set(Item arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		public void add(Item arg0) {
 			// TODO Auto-generated method stub
 			
 		}
--- src/linearList/DLinkedListMain.java
+++ src/linearList/DLinkedListMain.java
@@ -6,10 +6,29 @@ public class DLinkedListMain {
 		DLinkedList<Integer> DLinkList = new DLinkedList<Integer>();
 		
 		try{
-			DLinkList.addFirst(4);
-			DLinkList.addLast(8);
-			DLinkList.addFirst(10);
+			DLinkList.addFirst(2);
+			DLinkList.addLast(4);
+			DLinkList.addFirst(1);
+			DLinkList.add(2, 3);
+			DLinkList.addFirst(0);
 			System.out.println(DLinkList.output());
+			System.out.println(DLinkList.size()+"\n");
+
+			DLinkList.remove(2);
+			System.out.println(DLinkList.output());
+			System.out.println(DLinkList.size()+"\n");
+			
+			DLinkedList<Integer>.ListItr DLLIter = DLinkList.new ListItr();
+			while(DLLIter.hasNext()){
+				System.out.println(DLLIter.next());
+			}
+			while(DLLIter.hasPrevious()){
+				System.out.println(DLLIter.previous());
+			}
+
+
+
+
 		}
 		catch(Exception e){
 			System.out.println(e.getMessage());
