7bf574f7d9b289deb3c484130cab236874d99023
Miguel Angel <miguel.mzbi@gmail.com>
2016-09-10 20:40:23 -0500

    Finished H2

---
+++ src/h2/SparseMatrix.java
@@ -0,0 +1,161 @@
+package h2;
+
+import java.util.NoSuchElementException;
+
+public class SparseMatrix<Item> {
+	private int columns, rows;
+	public SMNode<Item>[] startNodes;
+	private Integer zero = 0;
+	
+	@SuppressWarnings("unchecked")
+	public SparseMatrix(Item[][] dArray){
+		this.rows = dArray.length;
+		this.columns = dArray[0].length;
+		this.startNodes = new SMNode[this.rows];
+		
+		for(int i = 0; i < this.rows; i++){
+			for(int j = 0; j < this.columns; j++){
+				if(dArray[i][j] != this.zero){
+					if(this.startNodes[i] == null){
+						this.startNodes[i] = new SMNode<Item>(dArray[i][j], i+1, j+1, null);
+					}
+					else{
+						SMNode<Item> temp = this.startNodes[i];
+						while(temp.next != null){
+							temp = temp.next;
+						}
+						temp.next = new SMNode<Item>(dArray[i][j], i+1, j+1, null);
+					}
+				}
+			}
+		}
+	}
+
+	public static class SMNode<Item>{
+		public Item item;
+		@SuppressWarnings("unused")
+		private int i,j;
+		private SMNode<Item> next;
+		
+		public SMNode(Item item, int i, int j, SMNode<Item> next){
+			this.item = item;
+			this.i = i;
+			this.j = j;
+			this.next = next;
+		}
+	}
+	
+	public void checkBounds(int i, int j){
+		if (i < 1 || i > this.rows|| j < 1 || j > this.columns){
+			throw new IndexOutOfBoundsException("Index must be in range");
+		}
+	}
+	
+	public void add(int i, int j, Item value){
+		checkBounds(i, j);
+		SMNode<Item> sNode = this.startNodes[i-1];
+		if(sNode == null){ //If no initial node is found, create new
+			this.startNodes[i-1] = new SMNode<Item>(value, i, j, null);
+		}
+		else{
+			if(sNode.j == j){ //Will become the new firstNode (First existed with other value)
+				sNode.item = value;
+			}
+			else if(sNode.j > j){ //Will become the new firstNode (firstNode was a bigger j)
+				this.startNodes[i-1] = new SMNode<Item>(value, i, j, sNode);
+			}
+			else{
+				while(sNode.next != null && sNode.next.j < j){ //If next node is not null (We are on the end) and the next j is bigger or equal, change to next node
+					sNode = sNode.next;
+				}
+				if(sNode.next != null && sNode.next.j == j){ //If next node has the same j and it's not null, change the value of that node
+					sNode.next.item = value;
+				}
+				else{ //If none of above, means next j is not smaller(Bigger or null) and current j is smaller. Node to be added on the middle
+					SMNode<Item> nxt = sNode.next;
+					sNode.next = new SMNode<Item>(value, i, j, nxt);
+				}
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Item get(int i, int j){
+		checkBounds(i, j);
+		SMNode<Item> sNode = this.startNodes[i-1];
+		
+		if(sNode == null){ //If no initial node is found, only zeroes on row
+			return (Item) zero;
+		}
+		else{
+			while(sNode.next != null && sNode.j != j){ //If next node is not null (We are on the end) and this node hasn't the needed j, change to next node
+				sNode = sNode.next;
+			}
+			if(sNode.j == j){ //The j was found on current node and returned (Wasn't a zero)
+				return sNode.item;
+			}
+			else{ //If next node is null, means the value is a zero (The j was not found on the nodes)
+				return (Item) zero;
+			}
+		}
+	}
+	
+	public Item remove(int i, int j){
+		checkBounds(i, j);
+		SMNode<Item> sNode = this.startNodes[i-1];
+		
+		if(sNode == null){ //If no initial node is not found, means only zeros on row. Nothing to remove
+			throw new NoSuchElementException("No item in index to remove");
+		}
+		else{
+			SMNode<Item> prv = null;
+			while(sNode.next != null && sNode.j != j){ //If next node is not null (We are on the end) and this node hasn't the needed j, change to next node
+				prv = sNode;
+				sNode = sNode.next;
+			}
+			
+			if(sNode.j == j){ //If the j was found on current node
+				if(prv == null){ //If the node is the first node
+					if(sNode.next == null){ //If the first node was the only on the row
+						this.startNodes[i-1] = null;
+					}
+					else{ //If the first node wasn't the only of the row
+						this.startNodes[i-1] = sNode.next;
+					}
+				}
+				else{ //If the node wasn't the first node on the row (Still it was found and is the current node), correct previous node's next
+					prv.next = sNode.next;
+				}
+				return sNode.item; //Return current node's item
+			}
+			else{
+				throw new NoSuchElementException("No item in index to remove");
+			}
+		}
+	}
+	
+	public String output(){
+		StringBuilder op = new StringBuilder();
+		SMNode<Item> temp;
+		for(int i = 0; i < this.rows; i++){
+			temp = this.startNodes[i];
+			
+			for(int j = 1; j <= this.columns; j++){
+				if(temp != null){
+					if(temp.j == j){
+						op.append(temp.item);
+						temp = temp.next;
+					}
+					else{
+						op.append(this.zero);
+					}
+				}
+				else{
+					op.append(this.zero);
+				}
+			}
+			op.append("\n");
+		}
+		return op.toString();
+	}
+}
---
+++ src/h2/SparseMatrixMain.java
@@ -0,0 +1,67 @@
+package h2;
+
+public class SparseMatrixMain {
+	public static void main(String[] args) {
+		Integer[][] intArray = {
+			{1,2,3,4,5,6,7,8,9},
+			{1,2,0,4,5,0,7,8,0},
+			{0,0,0,0,0,0,0,0,0},
+			{1,0,0,0,0,0,0,0,0},
+			{1,2,0,0,0,0,0,0,0},
+			{1,2,3,0,0,0,0,0,0},
+			{0,0,3,4,0,0,0,8,9}
+			};
+		SparseMatrix<Integer> SM = new SparseMatrix<Integer>(intArray);
+		
+		for(int j = 1; j <= 7; j++){
+			for(int i = 1; i <= 9; i++){
+				System.out.print(SM.get(j, i));
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		System.out.println(SM.remove(1, 1));
+		System.out.println(SM.output());
+		
+		System.out.println(SM.remove(1, 2));
+		System.out.println(SM.output());
+
+		System.out.println(SM.remove(1, 3));
+		System.out.println(SM.output());
+		
+		for(int j = 1; j <= 7; j++){
+			for(int i = 1; i <= 9; i++){
+				SM.add(j, i, 5);
+			}
+		}
+		System.out.println(SM.output());
+		
+		for(int j = 1; j <= 7; j++){
+			for(int i = 1; i <= 9; i++){
+				SM.remove(j, i);
+			}
+		}
+		System.out.println(SM.output());
+		
+		for(int j = 1; j <= 7; j++){
+			for(int i = 1; i <= 9; i++){
+				SM.add(j, i, 9);
+			}
+		}
+		System.out.println(SM.output());
+
+		for(int j = 1; j <= 7; j++){
+			SM.remove(j, j);
+		}
+		SM.remove(1, 8);
+		SM.remove(2, 9);
+		System.out.println(SM.output());
+		
+		try{
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
