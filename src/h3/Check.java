c3ae38388d2120d889967ef6b3bff2cd55264d4c
Miguel Angel <miguel.mzbi@gmail.com>
2016-09-18 20:30:01 -0500

    H3

---
+++ src/h3/Check.java
@@ -0,0 +1,50 @@
+package h3;
+
+public class Check {
+	
+	public static boolean Iterative(Chain<Character> c){
+		ChainStack<Character> ones = new ChainStack<Character>();
+		int s = c.size();
+		for(int i = 0; i < c.size(); i++){
+			if(c.get(i) == '1'){
+				ones.push(c.remove(i));
+				i--;
+			}
+		}
+		int noOnes = 0;
+		while(!ones.isEmpty()){
+			ones.pop();
+			noOnes++;
+		}
+		if(noOnes == c.size()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		Chain<Character> binaryChain = new Chain<Character>();
+		//10 zero, 7 one
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'1');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'1');
+		binaryChain.add(0,'1');
+		binaryChain.add(0,'1');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'1');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'1');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'0');
+		binaryChain.add(0,'1');
+		
+		System.out.println(Iterative(binaryChain));
+	}
+}
