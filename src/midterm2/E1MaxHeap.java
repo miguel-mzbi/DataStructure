8d3365d7826f275ea314d27163013e63e9fbd27d
Miguel A. Montoya Z. <miguel.mzbi@gmail.com>
2016-10-14 08:24:04 -0500

    E1

--- src/midterm2/E1MaxHeap.java
+++ src/midterm2/E1MaxHeap.java
@@ -15,41 +15,63 @@ public class E1MaxHeap {
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void put(Comparable theElement){
-		if(size == 0){
-			heap[1] = theElement;
+		if(size == 0){												//1
+			heap[1] = theElement;									//1
 		}
 		else{
-			heap[size+1] = theElement;
-			boolean heaped = false;
-			int child = size+1;
-			while(!heaped && child != 0){
-				int parent = child/2;
-				int cmp = heap[child].compareTo(heap[parent]);
-				if(cmp >= 0){
-					heap[child] = heap[parent];
-					child = parent;
+			heap[size+1] = theElement;								//1
+			boolean heaped = false;									//1
+			int child = size+1;										//1
+			while(!heaped && child > 1){							//l+1 (l = parents to inserted value. In the worst case, inserted value must become heap)
+				int parent = child/2;								//l
+				int cmp = heap[child].compareTo(heap[parent]);		//l
+				if(cmp >= 0){										//l
+					Comparable toParent = heap[child];				//l
+					heap[child] = heap[parent];						//l
+					heap[parent]= toParent;							//l
+					child = parent;									//l
 				}
 				else{
-					heaped = true;
+					heaped = true;									//l
 				}
 			}
 		}
-		size++;
+		size++;														//1
+	}
+	//Time complexity O(l) -> O(n)
+	
+	//Not necessary to answer. Necessary for testing
+	public void printHeap(){
+		for(int i = 1; i<= size; i++){
+			System.out.print(heap[i] +"-");
+		}
+		System.out.println("");
 	}
 	
 	public static void main(String[] args){
-		E1MaxHeap max = new E1MaxHeap(30);
+		E1MaxHeap max = new E1MaxHeap(15);
 		max.put(37);
+		max.printHeap();
 		max.put(20);
+		max.printHeap();
 		max.put(59);
+		max.printHeap();
 		max.put(4);
+		max.printHeap();
 		max.put(21);
+		max.printHeap();
 		max.put(53);
+		max.printHeap();
 		max.put(85);
+		max.printHeap();
 		max.put(1);
+		max.printHeap();
 		max.put(57);
+		max.printHeap();
 		max.put(61);
+		max.printHeap();
 		max.put(94);
+		max.printHeap();
 	}
 	
 }
