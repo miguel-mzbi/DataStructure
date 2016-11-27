a346f8a22cb644dc31436d603ff2b348a4aeec3f
Miguel Angel <miguel.mzbi@gmail.com>
2016-11-27 10:31:55 -0600

    Finished AddressTable Javadoc

--- src/projectDB/AddressTable.java
+++ src/projectDB/AddressTable.java
@@ -1,30 +1,69 @@
 package projectDB;
 
+/**
+ * <b>class AddressTable</b>
+ * <p>The purpose of this class is to store all the person entries of the Address Table (Table 1).
+ * It uses an AVL Tree data structure to store all the entries.
+ * <br>Each entry has a key (The name of the person) and a value (The address of the person).
+ * @author MiguelMontoya - ArturoFornes
+ */
 public class AddressTable {
-	//Data structure for table 1 Name - Address
-	//Each entry has a key (The name of the person) and a value (The address of the person)
+	
 	private int m;
 	private int size;
 	private Entry[] table;
 	
+	/**
+	 * <b>AddresTable</b>
+	 * <br><i>public AddressTable()</i>
+	 * <p>Constructor for the class AVLTreeMod.
+	 * <br>It initializes the table's size to the default value.
+	 */
 	public AddressTable(){
 		this(101);
 	}
 	
+	/**
+	 * <b>AddresTable</b>
+	 * <br><i>public AddressTable()</i>
+	 * <p>Constructor for the class AVLTreeMod.
+	 * <br>It initializes the table's size to the given capacity.
+	 * @param capacity - (Int) Integer with the desired capacity for the table.
+	 */
 	public AddressTable(int capacity){
 		this.m = capacity;
 		this.size = 0;
 		this.table = new Entry[capacity];
 	}
 	
+	/**
+	 * <b>hash</b>
+	 * <br><i>private int hash(String k)</i>
+	 * <p>Gives a hash value for the selected key
+	 * @param k - (String) Key to hash
+	 * @return Integer hash value for key
+	 */
 	private int hash(String k){
 		return (k.hashCode() & 0x7FFFFFFF) % this.m;
 	}
 	
+	/**
+	 * <b>hash</b>
+	 * <br><i>private int hash(String k, int m)</i>
+	 * <p>Gives a hash value for the selected key
+	 * @param k - (String) Key to hash
+	 * @param m - (Integer) New size of the table
+	 * @return Integer hash value for the key
+	 */
 	private int hash(String k, int m){
 		return (k.hashCode() & 0x7FFFFFFF) % m;
 	}
 	
+	/**
+	 * <b>resize</b>
+	 * <br><i>private void resize()</i>
+	 * <p>Creates new table, if max capacity is reached
+	 */
 	private void resize(){
 		Entry[] newTable = new Entry[this.m*2+1];
 		for(int i = 0; i < this.m; i++){
@@ -36,6 +75,15 @@ public class AddressTable {
 		this.table = newTable;
 	}
 	
+	/**
+	 * <b>add</b>
+	 * <br><i>public String add(String k, String item)</i>
+	 * <p>Adds a node<Key, Value> to the table.
+	 * If max is reached (Not capacity), rehashing is called before adding the new node<k, item>
+	 * @param k - (String) Key to add into node. It's the name of person.
+	 * @param item - (String) Item to add into node. It's the address of the person.
+	 * @return String, item (Address) of node if replaced
+	 */
 	public String add(String k, String item) {
 		if(this.size >= this.m-1 || (this.size+0.0)/(this.m) >= 0.75){
 			this.resize();
@@ -53,10 +101,16 @@ public class AddressTable {
 				return saved;
 			}
 		}
-		
 		return null;
 	}
 
+	/**
+	 * <b>remove</b>
+	 * <br><i>public String remove(String k)</i>
+	 * <p>Removes node with selected key (Person's name).
+	 * @param k - (String) Key to remove of the table. It's the person's name.
+	 * @return String, item of the removed entry. It's the person's address.
+	 */
 	public String remove(String k) {
 		int h = this.hash(k);
 		int freeSpace = -1;
@@ -73,7 +127,6 @@ public class AddressTable {
 					this.table[i] = null;
 					freeSpace = i;
 			}
-			
 		}
 		for(int i = freeSpace; freeSpace != -1; i = ++i%this.m){
 			if(i != freeSpace && this.table[i] == null){
@@ -91,6 +144,13 @@ public class AddressTable {
 		return saved;
 	}
 
+	/**
+	 * <b>getValue</b>
+	 * <br><i>public String getValue(String k)</i>
+	 * <p>Removes node with selected key (Person's name).
+	 * @param k - (String) Key of the entry to get value
+	 * @return Value of the entry with the desired key. Returns null if no entry is found.
+	 */
 	public String getValue(String k) {
 		int h = this.hash(k);
 		for(Entry n = table[h]; n != null; n = table[++h%this.m]){
@@ -101,28 +161,65 @@ public class AddressTable {
 		return null;
 	}
 
+	/**
+	 * <b>contains</b>
+	 * <br><i>public boolean contains(String k)</i>
+	 * <p>Checks for the existence of a key in the table.
+	 * @param k - (String) Key, person's name, to check if exists in the table.
+	 * @return Boolean, true if the key exists.
+	 */
 	public boolean contains(String k) {
 		return this.getValue(k) != null;
 	}
 
+	/**
+	 * <b>isEmpty</b>
+	 * <br><i>public boolean isEmpty()</i>
+	 * <p>Checks if the table is empty.
+	 * @return Boolean, true if table is empty.
+	 */
 	public boolean isEmpty() {
 		return this.size == 0;
 	}
 
+	/**
+	 * <b>getSize</b>
+	 * <br><i>public int getSize()</i>
+	 * <p>Obtains the number of entries in the table.
+	 * @return Integer, number of entries.
+	 */
 	public int getSize() {
 		return this.size;
 	}
 
+	/**
+	 * <b>clear</b>
+	 * <br><i>public void clear()</i>
+	 * <p>Removes every entry of the table.
+	 */
 	public void clear() {
 		for(int i = 0; i < this.m; i++){
 			this.table[i] = null;
 		}
 	}
 	
+	/**
+	 * <b>class Entry</b>
+	 * <br>Inner Class of AddressTable
+	 * <p>Each node (Entry) contains the name of the person as key for the node (Entry), and the address of the person.
+	 * @author MiguelMontoya - ArturoFornes
+	 */
 	private static class Entry{
 		String key;
 		String value;
 		
+		/**
+		 * <b>Entry</b>
+		 * <br><i>private Entry(String key, String value)</i>
+		 * <p>Constructor for the inner class Entry
+		 * @param key - (String) Key of the node. It's the person's name.
+		 * @param value - (String) Value of the node. It's the person's address.
+		 */
 		private Entry(String key, String value){
 			this.key = key;
 			this.value = value;
