147dac18abbdbcd0d0ae8a5e51bb3d5aca4a98bd
Miguel A. Montoya Z. <miguel.mzbi@gmail.com>
2016-11-27 19:43:16 -0600

    Finished Edge javadoc

--- src/projectDB/Edge.java
+++ src/projectDB/Edge.java
@@ -1,9 +1,23 @@
 package projectDB;
 
+/**
+ * <b>class Edge</b>
+ * <p>The purpose of this class is to create edges for a graph.
+ * Each edge has always a destination (The personB's name) 
+ * and a cost (The expense difference between the person who owns the edge and the personB).
+ * @author MiguelMontoya - ArturoFornes
+ */
 public class Edge {
 	Vertex destination;
 	double cost;
 	
+	/**
+	 * <b>Edge</b>
+	 * <br><i>public Edge(Vertex destination, double cost)</i>
+	 * <p>Constructor for the class Edge.
+	 * @param destination - (Vertex) Destination Vertex
+	 * @param cost - (Double) Cost, expense difference between the people.
+	 */
 	public Edge(Vertex destination, double cost){
 		this.destination = destination;
 		this.cost = cost;
