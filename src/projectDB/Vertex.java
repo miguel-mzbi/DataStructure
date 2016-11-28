35e2e8895c9c78763055a7bf69f345013a529a48
Miguel A. Montoya Z. <miguel.mzbi@gmail.com>
2016-11-27 19:40:11 -0600

    Finished Vertex Javadoc

--- src/projectDB/Vertex.java
+++ src/projectDB/Vertex.java
@@ -3,6 +3,12 @@ package projectDB;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <b>class Vertex/b>
+ * <p>The purpose of this class is to create vertexes for a graph.
+ * Each vertex has always a name (The person name) and a list of edges to other vertex.
+ * @author MiguelMontoya - ArturoFornes
+ */
 public class Vertex {
 	String name;
 	List<Edge> adjList;
@@ -10,10 +16,17 @@ public class Vertex {
 	double distance;
 	Vertex pi;
 	
+	/**
+	 * <b>Vertex</b>
+	 * <br><i>public Vertex(String name)</i>
+	 * <p>Constructor for the class Vertex.
+	 * @param name - (String) Name of the person
+	 */
 	public Vertex(String name){
 		this.name = name;
 		adjList = new ArrayList<Edge>();
 	}
+	
 	
 	public String toString(){
 		return this.name;
